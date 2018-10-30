99aa6bf171045f5b9006fa2ed4ecd1176f46aa9b
Jonas Strømsodd <jonas.stromsodd@finn.no>
2018-10-30 13:13:48 +0100

    Update key mapping script

--- remap_escape_and_prtscr.sh
+++ remap_escape_and_prtscr.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 
-xmodmap -e "clear lock" #disable caps lock switch              
+xmodmap -e "clear lock" #disable caps lock switch
 xmodmap -e "keysym Caps_Lock = Escape" #set caps_lock as escape
 xmodmap -e "keysym Print = Super_R"
+xset r rate 300 30
