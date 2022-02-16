1e838c95a207c71136c55278d98fa9f8ed41f7a2
Jonas Strømsodd <jonas.stromsodd@gmail.com>
2022-02-16 10:30:18 +0100

    Rewrite bootstrap script and add bat config

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,46 +1,12 @@
-#!/usr/bin/zsh
+#!/usr/bin/bash
 
-link_file () {
-    dotfile=$1
-    base=$(basename $dotfile)
-    directory=$HOME
+GIT_ROOT=$(git rev-parse --show-toplevel)
 
-    if [[ -n $2 ]]; then
-        directory=$2
-    fi
+ln -s $GIT_ROOT/vim/.vimrc ~/.vimrc
+ln -s $GIT_ROOT/tig/.tigrc ~/.tigrc
+ln -s $GIT_ROOT/ripgrep/.ripgreprc ~/.ripgreprc
 
-    location=$directory/$base
+mkdir -p ~/.config/bat
+ln -s bat/config $HOME/.config/bat/config
 
-
-    if [[ -f $location ]]; then
-        if [[ ! -L $location ]]; then
-            echo "Backing up ${location} to ${location}.backup"
-            mv $location $location.backup
-        fi
-    fi
-
-    if [[ $location -ef $dotfile ]]; then
-        echo "dotfiles/${dotfile} already symlinked into ${location}"
-        continue
-    fi
-
-    cd $directory
-    echo "Symlinking dotfiles/${dotfile} into ${location}"
-
-    src=dotfiles/$dotfile
-    if [[ -n $3 ]]; then
-        src=$3$src
-    fi
-    ln -s $src $base
-    cd -
-}
-
-# Files that go in $HOME
-for dotfile (zsh/.* vim/.*); do
-    link_file $dotfile
-done
-
-# Atom configuration Files
-for dotfile (atom/*.js atom/*.json); do
-    link_file $dotfile $HOME/.atom "../"
-done
+sudo apt-get install -y tig ripgrep bat
