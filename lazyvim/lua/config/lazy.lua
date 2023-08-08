820ba0be3b65927561fcbc656fee51b1c1ac587a
Jonas Strømsodd <jonas.stromsodd@gmail.com>
2023-08-08 08:19:46 +0200

    Update layzvim

--- lazyvim/lua/config/lazy.lua
+++ lazyvim/lua/config/lazy.lua
@@ -22,7 +22,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.json" },
     -- import any extras modules here
     { import = "lazyvim.plugins.extras.lang.docker" },
-    { import = "lazyvim.plugins.extras.formatting.prettier" },
+    -- { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.util.project" },
