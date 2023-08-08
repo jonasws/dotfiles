17dc8dc1219688d5e7bdae89445fb8929b04bbd0
Jonas Strømsodd <jonas.stromsodd@gmail.com>
2023-08-08 13:56:32 +0200

    Update schmupdate lazy

--- lazyvim/lua/config/lazy.lua
+++ lazyvim/lua/config/lazy.lua
@@ -22,13 +22,14 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.json" },
     -- import any extras modules here
     { import = "lazyvim.plugins.extras.lang.docker" },
-    -- { import = "lazyvim.plugins.extras.formatting.prettier" },
+    { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.util.project" },
     { import = "lazyvim.plugins.extras.coding.yanky" },
     { import = "lazyvim.plugins.extras.editor.mini-files" },
-    -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
+    { import = "lazyvim.plugins.extras.editor.leap" },
+    { import = "lazyvim.plugins.extras.ui.mini-animate" },
     { import = "lazyvim.plugins.extras.vscode" },
     -- import/override with your plugins
     { import = "plugins" },
--- lazyvim/lua/plugins/mini.lua
+++ lazyvim/lua/plugins/mini.lua
@@ -1,13 +1,5 @@
 return {
   {
-    "echasnovski/mini.indentscope",
-    opts = {
-      draw = {
-        animation = require("mini.indentscope").gen_animation.none(),
-      },
-    },
-  },
-  {
     "echasnovski/mini.files",
     opts = {
       windows = {
