a874293788ca3d4c6e3fd722db12fc46bd967525
Jonas Strømsodd <jonas.stromsodd@gmail.com>
2023-08-25 15:21:54 +0200

    Update schmupdate swchupdate

--- lazyvim/lua/plugins/null-ls.lua
+++ lazyvim/lua/plugins/null-ls.lua
@@ -10,6 +10,19 @@ return {
     opts = function(_, opts)
       local nls = require("null-ls")
       table.insert(opts.sources, nls.builtins.formatting.prettier)
+
+      -- Remove shfmt from opts.sources
+      for i, source in ipairs(opts.sources) do
+        if source.name == "shfmt" then
+          table.remove(opts.sources, i)
+        end
+      end
+      table.insert(
+        opts.sources,
+        nls.builtins.formatting.shfmt.with({
+          extra_args = { "--indent", "2", "--case-indent", "--space-redirects" },
+        })
+      )
     end,
   },
 }
