b8934e3f7db2e4aff3c4ff5133d257d4e2015b54
Jonas Strømsodd <jonas.stromsodd@gmail.com>
2023-09-17 10:27:49 +0200

    Update lazyvim

--- lazyvim/lua/config/lazy.lua
+++ lazyvim/lua/config/lazy.lua
@@ -31,6 +31,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.ui.mini-animate" },
     { import = "lazyvim.plugins.extras.vscode" },
     { import = "lazyvim.plugins.extras.lang.terraform" },
+    { import = "lazyvim.plugins.extras.coding.copilot" },
     -- import/override with your plugins
     { import = "plugins" },
   },
