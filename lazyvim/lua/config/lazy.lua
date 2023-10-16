2785e36faacba93c57f8dce20bab9e4449b497fd
Jonas Strømsodd <jonas.stromsodd@gmail.com>
2023-10-16 08:13:01 +0200

    Update lazyvim

--- lazyvim/lua/config/lazy.lua
+++ lazyvim/lua/config/lazy.lua
@@ -31,7 +31,8 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.ui.mini-animate" },
     { import = "lazyvim.plugins.extras.vscode" },
     { import = "lazyvim.plugins.extras.lang.terraform" },
-    -- { import = "lazyvim.plugins.extras.coding.copilot" },
+    { import = "lazyvim.plugins.extras.lsp.none-ls" },
+    { import = "lazyvim.plugins.extras.formatting.prettier" },
     -- import/override with your plugins
     { import = "plugins" },
   },
--- lazyvim/lua/plugins/none-ls.lua
+++ lazyvim/lua/plugins/none-ls.lua
@@ -1,15 +1,8 @@
 return {
   {
-    "williamboman/mason.nvim",
-    opts = function(_, opts)
-      table.insert(opts.ensure_installed, "prettier")
-    end,
-  },
-  {
     "nvimtools/none-ls.nvim",
     opts = function(_, opts)
       local nls = require("null-ls")
-      table.insert(opts.sources, nls.builtins.formatting.prettier)
 
       -- Remove shfmt from opts.sources
       for i, source in ipairs(opts.sources) do
