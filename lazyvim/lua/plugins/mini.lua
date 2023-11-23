42b556a8cab396a1c6b8428be06534f0c20b80a6
Jonas Strømsodd <jonasws@hey.com>
2023-11-23 16:07:17 +0100

    Update lazyvim config, including lockfile

---
+++ lazyvim/lua/plugins/mini.lua
@@ -0,0 +1,10 @@
+return {
+  "echasnovski/mini.files",
+  opts = {
+    windows = {
+      preview = true,
+      preview_focus = 60,
+      width_preview = 60,
+    },
+  },
+}
--- lazyvim/lua/plugins/telescope.lua
+++ lazyvim/lua/plugins/telescope.lua
@@ -11,22 +11,6 @@ return {
         require("telescope").load_extension("fzf")
       end,
     },
-    keys = {
-      {
-        "<leader>fp",
-        function()
-          local git_dir = vim.fn.systemlist("git rev-parse --show-toplevel")
-
-          -- Use nil if the previous command failed
-          if vim.v.shell_error ~= 0 then
-            Util.telescope("find_files", {})()
-          else
-            Util.telescope("find_files", { cwd = git_dir[1] })()
-          end
-        end,
-        desc = "Find project files",
-      },
-    },
     opts = {
       defaults = {
         mappings = {
