bdca4b3bdfccd7214eb9587515a00f89a65a0a43
Jonas Strømsodd <jonas.stromsodd@gmail.com>
2023-12-07 20:11:33 +0100

    Update options.lua

--- lazyvim/lua/config/options.lua
+++ lazyvim/lua/config/options.lua
@@ -1,8 +1,3 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.g.node_host_prog = "/Users/jonasws/Library/Caches/fnm_multishells/24689_1699519900266/bin/neovim-node-host"
-
-vim.opt.foldmethod = "expr"
-vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
-vim.opt.foldenable = false
