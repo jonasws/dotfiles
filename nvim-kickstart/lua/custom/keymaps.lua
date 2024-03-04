d19ed37f08c2c6ffe4eecb84ac50261df388874f
Jonas Strømsodd <jonasws@hey.com>
2024-03-04 13:14:51 +0100

    Add neovim kickstart config

---
+++ nvim-kickstart/lua/custom/keymaps.lua
@@ -0,0 +1,3 @@
+vim.keymap.set('n', '<leader>qq', ':qa<Enter>', { desc = 'Quit Neovim' })
+vim.keymap.set('c', '<C-j>', '<Down>', { noremap = true, desc = 'Down' })
+vim.keymap.set('c', '<C-k>', '<Up>', { noremap = true, desc = 'Up' })
