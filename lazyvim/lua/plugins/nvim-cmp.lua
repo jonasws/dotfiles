74d3dbb20fb8940b7d9f96e49b115a8a57956255
Jonas Strømsodd <jonasws@hey.com>
2024-04-08 13:48:58 +0200

    Update lazyvim setup

--- lazyvim/lua/plugins/nvim-cmp.lua
+++ lazyvim/lua/plugins/nvim-cmp.lua
@@ -22,7 +22,6 @@ return {
         return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
       end
 
-      local luasnip = require("luasnip")
       local cmp = require("cmp")
       local types = require("cmp.types")
 
