07759bff4c4c4239e0bd4081878e7cd830ac8496
Jonas Strømsodd <2318955+jonasws@users.noreply.github.com>
2024-07-01 14:06:45 +0200

    Update kickstart setup
      Include oil and regal plugins

--- nvim-kickstart/init.lua
+++ nvim-kickstart/init.lua
@@ -547,7 +547,7 @@ require('lazy').setup {
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         -- clangd = {},
-        -- gopls = {},
+        gopls = {},
         -- pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
@@ -557,6 +557,9 @@ require('lazy').setup {
         --
         -- But for many setups, the LSP (`tsserver`) will work just fine
         tsserver = {},
+        regal = {
+          root_dir = require('lspconfig.util').root_pattern '.git',
+        },
         --
 
         lua_ls = {
@@ -615,6 +618,7 @@ require('lazy').setup {
           end,
         },
       }
+      require('lspconfig').regal.setup(servers.regal)
     end,
   },
 
---
+++ nvim-kickstart/lua/custom/plugins/oil.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    'stevearc/oil.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      require('oil').setup {
+        columns = { 'icon' },
+        keymaps = {
+          ['<C-h>'] = false,
+          ['<M-h>'] = 'actions.select_split',
+        },
+        view_options = {
+          show_hidden = true,
+        },
+        experimental_watch_for_changes = true,
+        delete_to_trash = false,
+      }
+
+      -- Open parent directory in current window
+      vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
+
+      -- Open parent directory in floating window
+      vim.keymap.set('n', '<space>-', require('oil').toggle_float, { desc = 'Open parent directory (floating)' })
+    end,
+  },
+}
