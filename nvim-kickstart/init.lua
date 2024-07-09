2d54244fc2d989bb1535dfcea76449195b75bad4
Jonas Strømsodd <2318955+jonasws@users.noreply.github.com>
2024-07-09 14:25:02 +0200

    Update kickstart setup

--- nvim-kickstart/init.lua
+++ nvim-kickstart/init.lua
@@ -105,7 +105,7 @@ vim.opt.foldenable = false
 vim.opt.number = true
 -- You can also add relative line numbers, for help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
+vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
@@ -559,6 +559,7 @@ require('lazy').setup {
         --
         -- But for many setups, the LSP (`tsserver`) will work just fine
         tsserver = {},
+        eslint = {},
         yamlls = {},
         gitlab_ci_ls = {},
         regal = {
