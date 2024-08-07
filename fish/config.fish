b59008f0e12996fbba61a38317619f4590555545
Jonas Strømsodd <2318955+jonasws@users.noreply.github.com>
2024-08-07 16:17:45 +0200

    Update fish setup for entur shorthands

--- fish/config.fish
+++ fish/config.fish
@@ -333,6 +333,10 @@ function start-my-day
     nvim --headless "+Lazy! sync" +qa
     echo
 
+    echo "Updating your Mason tools"
+    nvim --headless "+MasonUpdate" +qa
+    echo
+
     echo "Updating intellimacs"
     git -C ~/.intellimacs pull --rebase
 end
@@ -340,10 +344,10 @@ end
 
 function nittedal
     set -l query '
-query GetDepartures {
-  stopPlace(id: "NSR:StopPlace:337") {
-    estimatedCalls(whiteListed: {
-            lines: ["GJB:Line:R30", "GJB:Line:L3"]
+query GetDepartures($stopPlace: String!, $lines: [ID!]!, $timeRange: Int = 86400, $numberOfDepartures: Int = 5, $startTime: DateTime!) {
+  stopPlace(id: $stopPlace) {
+    estimatedCalls(timeRange: $timeRange, numberOfDepartures: $numberOfDepartures, startTime: $startTime, whiteListed: {
+            lines: $lines,
     }) {
       expectedDepartureTime
       situations {
@@ -367,22 +371,38 @@ query GetDepartures {
   }
 }
   '
+    set -l numberOfDepartures $argv[1]
 
-    http --ignore-stdin POST https://api.entur.io/journey-planner/v3/graphql query=$query "ET-Client-Name: jonas-laptop-cli" \
+    if test -n "$argv[2]"
+        set -l parsedTime (string split : $argv[2]) 
+        set -f startTime (date -Iminutes -v$parsedTime[1]H -v$parsedTime[2]M)
+    else
+        set -f startTime (date -Iminutes)
+    end
+
+    set -l variables (jq -n --arg numberOfDepartures $numberOfDepartures --arg startTime $startTime '
+    {
+        stopPlace: "NSR:StopPlace:59872",
+        lines: ["GJB:Line:R30", "GJB:Line:L3"],
+        numberOfDepartures: $numberOfDepartures | tonumber,
+        startTime: $startTime
+    } | @json')
+
+    http --ignore-stdin POST https://api.entur.io/journey-planner/v3/graphql variables:=$variables query=$query "ET-Client-Name: jonas-laptop-cli" \
         | jq -r '
             .data.stopPlace.estimatedCalls[] 
             | [.expectedDepartureTime, .destinationDisplay.frontText, .quay.publicCode, .serviceJourney.line.publicCode, .situations[0].summary[0].value]
             | @tsv' \
         # Only show the time, including display Norwegian characters
         | perl -Mutf8 -CS -ne 'print "\e[34m$2\e[0m\t\e[96m$3\e[0m\t\e[92m$4\e[0m\t\e[95m$5\e[0m\t\e[93m$6\e[0m\n"
-              if /(\d{4}-\d{2}-\d{2}T)(\d{2}:\d{2}):\d{2}\+\d{2}:\d{2}\t(\p{L}+)\t(\d+)\t(.+)\t(.*)/' \
+              if /(\d{4}-\d{2}-\d{2}T)(\d{2}:\d{2}):\d{2}\+\d{2}:\d{2}\t(\p{L}+)\t(\d+)?\t(.+)\t(.*)/' \
         | column -t -s (echo -n \t)
 end
 
 function xray-traceid
     set -l traceId $argv[1]
     if test -z $traceId
-        set -a traceId (uuidgen | string lower)
+        set traceId (uuidgen | string lower)
     end
     printf 'Root=1-%x-%s' (date +%s) (string replace -a "-" "" $traceId)
 end
@@ -418,3 +438,4 @@ set -gx AWS_DEFAULT_OUTPUT json
 [ -f ~/.config/tabtab/fish/__tabtab.fish ]; and . ~/.config/tabtab/fish/__tabtab.fish; or true
 
 source ~/.config/op/plugins.sh
+
