18da66914d6fb4475b9d33ef838638ac82b1cfbe
Jonas Strømsodd <2318955+jonasws@users.noreply.github.com>
2024-08-12 14:07:47 +0200

    Update neovim kickstart

--- nvim-kickstart/init.lua
+++ nvim-kickstart/init.lua
@@ -166,6 +166,25 @@ vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
 
+vim.api.nvim_create_user_command('FormatDisable', function(args)
+  if args.bang then
+    -- FormatDisable! will disable formatting just for this buffer
+    vim.b.disable_autoformat = true
+  else
+    vim.g.disable_autoformat = true
+  end
+end, {
+  desc = 'Disable autoformat-on-save',
+  bang = true,
+})
+
+vim.api.nvim_create_user_command('FormatEnable', function()
+  vim.b.disable_autoformat = false
+  vim.g.disable_autoformat = false
+end, {
+  desc = 'Re-enable autoformat-on-save',
+})
+
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
 -- is not what someone will guess without a bit more experience.
@@ -313,7 +332,7 @@ require('lazy').setup {
   { -- Fuzzy Finder (files, lsp, etc)
     'nvim-telescope/telescope.nvim',
     event = 'VimEnter',
-    branch = '0.1.x',
+    branch = 'master',
     dependencies = {
       'nvim-lua/plenary.nvim',
       { -- If encountering errors, see telescope-fzf-native README for install instructions
@@ -649,10 +668,13 @@ require('lazy').setup {
     'stevearc/conform.nvim',
     opts = {
       notify_on_error = false,
-      format_on_save = {
-        timeout_ms = 500,
-        lsp_fallback = true,
-      },
+      format_on_save = function(bufnr)
+        -- Disable with a global or buffer-local variable
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+          return
+        end
+        return { timeout_ms = 500, lsp_fallback = true }
+      end,
       formatters_by_ft = {
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,47 +1,48 @@
-local wezterm = require("wezterm")
+local wezterm = require 'wezterm'
 local act = wezterm.action
 
-local config = {}
+local config = wezterm.config_builder()
 
-config.font = wezterm.font("JetBrainsMono Nerd Font")
+config.font = wezterm.font 'JetBrainsMono Nerd Font'
+config.default_prog = { '/Users/jonasws/.local-fish/bin/fish', '-l' }
 
-config.color_scheme = "Dracula (Official)"
+config.color_scheme = 'Dracula (Official)'
 config.tab_bar_at_bottom = true
 config.use_fancy_tab_bar = false
-config.window_decorations = "RESIZE"
+config.window_decorations = 'RESIZE'
 config.font_size = 16.0
 
 config.keys = {
-	{
-		key = "d",
-		mods = "SUPER",
-		action = act.SplitVertical({ domain = "CurrentPaneDomain" }),
-	},
-	{
-		key = "r",
-		mods = "SUPER",
-		action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }),
-	},
-	{
-		key = "]",
-		mods = "SUPER",
-		action = act.ActivatePaneDirection("Next"),
-	},
-	{
-		key = "[",
-		mods = "SUPER",
-		action = act.ActivatePaneDirection("Prev"),
-	},
-	{
-		key = ".",
-		mods = "SUPER",
-		action = act.PaneSelect,
-	},
-	{
-		key = "p",
-		mods = "SUPER",
-		action = wezterm.action.ActivateCommandPalette,
-	},
+  {
+    key = 'd',
+    mods = 'SUPER',
+    action = act.SplitVertical { domain = 'CurrentPaneDomain' },
+  },
+  {
+    key = 'r',
+    mods = 'SUPER',
+    action = act.SplitHorizontal { domain = 'CurrentPaneDomain' },
+  },
+  {
+    key = ']',
+    mods = 'SUPER',
+    action = act.ActivatePaneDirection 'Next',
+  },
+  {
+    key = '[',
+    mods = 'SUPER',
+    action = act.ActivatePaneDirection 'Prev',
+  },
+  {
+    key = '.',
+    mods = 'SUPER',
+    action = act.PaneSelect,
+  },
+  {
+    key = 'p',
+    mods = 'SUPER',
+    action = wezterm.action.ActivateCommandPalette,
+  },
 }
 
 return config
